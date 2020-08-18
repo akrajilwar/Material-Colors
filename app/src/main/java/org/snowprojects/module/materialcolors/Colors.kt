2a58b31e4ced28b4c3432336bf380f4dc3655064
Akshay Raj <snowcorp2@gmail.com>
2020-08-18 11:34:10 +0530

    Initial commit

---
+++ app/src/main/java/org/snowprojects/module/materialcolors/Colors.kt
@@ -0,0 +1,11 @@
+package org.snowprojects.module.materialcolors
+
+/**
+ * Created by Snow Corp Team on 17/08/20 at 9:38 AM.
+ * support@snowcorp.org
+ * www.snowcorp.org
+ */
+
+object Colors {
+    val RED_50 = ""
+}
